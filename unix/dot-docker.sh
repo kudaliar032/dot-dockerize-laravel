b03f5276a30c26424ae089cbf8c1f4d4b09490a1
Aditya Rahman <adityarahman032@gmail.com>
2019-07-14 13:48:28 +0800

    update script

--- unix/dot-docker.sh
+++ unix/dot-docker.sh
@@ -4,17 +4,13 @@ case $1 in
   clean) docker-compose down;;
   start) docker-compose start;;
   stop) docker-compose stop;;
-  webapp) docker-compose exec webapp $2;;
-  database) docker-compose exec database $2;;
-  redis) docker-compose exec redi $2;;
+  webapp|database|redis) docker-compose exec $@;;
   help)
-    echo "dot-docker build, to build all container services"
-    echo "dot-docker clean, to stop and clean all container services"
-    echo "dot-docker start, to start all services"
-    echo "dot-docker stop, to stop all services without clean container"
-    echo "dot-docker webapp [COMMAND], to run bash command in webapp service"
-    echo "dot-docker database [COMMAND], to run bash command in database service"
-    echo "dot-docker redis [COMMAND], to run bash command in redis service"
+    echo "dot-docker build - build all container services"
+    echo "dot-docker clear - stop and delete all container services"
+    echo "dot-docker start - start all services"
+    echo "dot-docker stop - stop all services without delete container"
+    echo "dot-docker [SERVICE] [COMMAND] - run bash command in a service"
   ;;
-  *) echo "dot-docker help, for help command list";;
+  *) echo "dot-docker help, command list help";;
 esac
